463ddb1100c553371f448c5fae2c9cf00eed98f7
Frane Bandov <frane@offbyte.com>
2026-06-26 14:39:43 +0200

    Bump vibesurfer cask to v0.1.18

--- Casks/vibesurfer.rb
+++ Casks/vibesurfer.rb
@@ -1,16 +1,16 @@
 # Hand-authored. Future versions will regenerate this from the
 # release-pipeline outputs to mirror agented's GoReleaser flow.
 cask "vibesurfer" do
-  version "0.1.17"
+  version "0.1.18"
 
   on_macos do
     on_arm do
-      sha256 "8aefb86766a743eb17d90e8031859e102a9e485769861881fe59b977ff97afc8"
+      sha256 "0a5d7661b1450ea1efd1eee72b049eef7caa7bc5768914914d93db00df32b896"
       url "https://github.com/frane/vibesurfer/releases/download/v#{version}/vs-v#{version}-aarch64-apple-darwin.tar.gz",
         verified: "github.com/frane/vibesurfer"
     end
     on_intel do
-      sha256 "92c3da0e11dbd225688e8673a6a648eaa066b19842930056e1892f7e8e08431f"
+      sha256 "b07e477fbdea8e9d4874f018767e936f8e6d7b6fb5db4a746198b9422323a763"
       url "https://github.com/frane/vibesurfer/releases/download/v#{version}/vs-v#{version}-x86_64-apple-darwin.tar.gz",
         verified: "github.com/frane/vibesurfer"
     end
@@ -18,7 +18,7 @@ cask "vibesurfer" do
 
   on_linux do
     on_intel do
-      sha256 "409443adb3529bb3b44fd602241903b257a329e480c15fa8a19ddf890d2bae73"
+      sha256 "206519a5d296de8b5e99e95bfe97b538fef1a8975b15037186f8d350ab8ecfb3"
       url "https://github.com/frane/vibesurfer/releases/download/v#{version}/vs-v#{version}-x86_64-unknown-linux-gnu.tar.gz",
         verified: "github.com/frane/vibesurfer"
     end
